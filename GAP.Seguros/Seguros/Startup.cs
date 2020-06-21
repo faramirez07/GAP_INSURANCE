3a07f98e95baffc3bda3a6a1f9b92c1f6114a2af
framirez07 <faramirez07@hotmail.com>
2020-06-20 23:05:10 -0600

    Implementación de Seguridad
    Se implementa la seguridad paa ingresar al sitio web.

---
+++ GAP.Seguros/Seguros/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Seguros.Startup))]
+namespace Seguros
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
