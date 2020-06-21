f8d5bfe2f8d7fa32af52fee9252d77bc5d2d59af
framirez07 <faramirez07@hotmail.com>
2020-06-21 15:48:53 -0600

    Creacion Servicio
    Creacion de Servicio

---
+++ GAP.Seguros/GAP.Seguros.Servicio/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace GAP.Seguros.Servicio.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ GAP.Seguros/GAP.Seguros.Servicio/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GAP.Seguros.Servicio.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
